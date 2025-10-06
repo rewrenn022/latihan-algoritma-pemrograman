ebddbdd30a90f03ce8db834a7989f08f995767af
rewrenn022 <renatapratiwi63@gmail.com>
2025-10-07 05:51:54 +0700

    Add files via upload

---
+++ latihan_1.cpp
@@ -0,0 +1,19 @@
+/*
+Nama         : Renata Pratiwi
+NPM          : 140810250010
+Kelas        : B
+Waktu Pengerjaan : 5 Menit
+*/
+#include <iostream>
+using namespace std;
+
+int main (){
+    int N;
+    cin >> N;
+    if (N > 0){
+        cout << "Postif" << endl;
+    } else if (N < 0) {
+        cout << "Negatif" << endl;
+    } else if (N = 0) 
+        cout << "Nol" << endl;
+}
